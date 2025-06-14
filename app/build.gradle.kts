d861514864cb7d5d75718f3423d06077d6b4f36a
sunny <128305546+sunnyygo@users.noreply.github.com>
2025-06-14 08:48:55 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.firebase.crashlytics)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
